04e17b98fbe4deea49577b4c1d8dc6a3204a2859
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-19 10:59:49 -0300

    Category

---
+++ src/main/java/com/guilherme/course/resources/CategoryResource.java
@@ -0,0 +1,33 @@
+package com.guilherme.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.course.entities.Category;
+import com.guilherme.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> users = categoryService.findAll();
+		return ResponseEntity.ok(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category user = categoryService.findById(id);
+		return ResponseEntity.ok(user);
+	}
+}
