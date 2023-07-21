16e9c1c1e87a0be9df2cf09210e4a98a0d91b780
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-21 12:43:36 -0300

    Exception handling - delete

--- src/main/java/com/guilherme/course/services/UserService.java
+++ src/main/java/com/guilherme/course/services/UserService.java
@@ -1,43 +1,52 @@
 package com.guilherme.course.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.course.entities.User;
 import com.guilherme.course.repositories.UserRepository;
+import com.guilherme.course.services.exceptions.DatabaseException;
 import com.guilherme.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException());
 	}
-	
+
 	public User insert(User user) {
 		return repository.save(user);
 	}
-	
+
 	public void delete(Long id) {
-		User user = repository.getReferenceById(id);
-		repository.delete(user);
+		try {
+			User user = repository.findById(id).get();
+			repository.delete(user);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		} catch(NoSuchElementException e) {
+			throw new ResourceNotFoundException();
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User user = repository.getReferenceById(id);
 		updateUser(user, obj);
 		return repository.save(user);
 	}
-	
+
 	private void updateUser(User user, User obj) {
 		user.setEmail(obj.getEmail());
 		user.setName(obj.getName());
